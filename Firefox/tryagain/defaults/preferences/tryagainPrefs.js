ce7810430527d305e346b957be7a2ed615865b54
Paul Lammertsma <paul@pixplicity.com>
2010-12-10 19:39:12 +0000

    New submission for 3.4.4, fixing problem for Fx2.0 and older

--- Firefox/tryagain/defaults/preferences/tryagainPrefs.js
+++ Firefox/tryagain/defaults/preferences/tryagainPrefs.js
@@ -5,5 +5,6 @@ pref("extensions.tryagain.repeat", 5);
 pref("extensions.tryagain.showmenu", 0);
 pref("extensions.tryagain.hidetips", 0);
 pref("extensions.tryagain.useauditing", 1);
+pref("extensions.tryagain.auditingTimeout", 60);
 pref("extensions.tryagain.showCaches", 1);
 pref("extensions.tryagain.customStyle", 0);
