2f689f080ebd483af71ebe8bddbe259b4aa763e6
Paul Lammertsma <paul@pixplicity.com>
2010-12-13 18:26:58 +0000

    Chrome version almost ready

---
+++ Chrome/tryagain.js
@@ -0,0 +1,73 @@
+const RETRY_CANCEL = 0;
+const RETRY_NORMAL = 1;
+const RETRY_OTHER  = 2;
+const RETRY_NONE   = 3;
+
+var auto_retry      = RETRY_NORMAL;
+var seconds         = 3;
+
+document.onkeydown = keyPress;
+
+function keyPress(e) {
+  if (e.keyCode == 27) {
+    stopRetry();
+  }
+}
+
+function autoRetryThis() {
+  var countdown = this.document.getElementById('countdown');
+  if (auto_retry == RETRY_NORMAL) {
+    if (seconds <= 0) {
+      var action = this.document.getElementById('action');
+      action.innerHTML = "try again!";
+      retryThis(null);
+    } else {
+      countdown.innerHTML += seconds + "... ";
+      setTimeout("autoRetryThis()", 1000);
+      seconds--;
+    }
+  } else if (auto_retry == RETRY_CANCEL) {
+    countdown.innerHTML += "Canceled.";
+    auto_retry = RETRY_NONE;
+  }
+}
+
+function retryThis(btn) {
+  // Session history has the URL of the page that failed
+  // to load, not the one of the error page. So, just call
+  // reload(), which will also repost POST data correctly.
+  try {
+    var url = this.document.getElementById('errorRetry').value;
+    // Reload doesn't work in Chrome
+    //location.reload();
+    // Just pass along the original URL; POST data will be dropped :(
+    location.href = url;
+  } catch (e) {
+    // We probably tried to reload a URI that caused an exception to
+    // occur;    e.g. a non-existent file.
+  }
+  if (!btn) {
+    btn = document.getElementById("errorTryAgain");
+  }
+  if (btn) {
+    btn.disabled = true;
+  }
+  document.getElementById("errorStopRetry").disabled = false;
+}
+
+function retryThisExtended() {
+  auto_retry = RETRY_OTHER;
+  retryThis(null);
+}
+
+function stopRetry() {
+  window.stop();
+  if (auto_retry == RETRY_NORMAL) {
+    auto_retry = RETRY_CANCEL;
+  }
+  autoRetryThis();
+  var errorStopRetry = this.document.getElementById('errorStopRetry');
+  errorStopRetry.setAttribute("disabled", "disabled");
+}
+
+setTimeout("autoRetryThis()", 100);
--- Chrome/tryagain_content_script.js
+++ Chrome/tryagain_content_script.js
@@ -1,6 +1,124 @@
-chrome.extension.sendRequest({attempt: 1},
-  function(response) {
-    $('#content').html("TryAgain!");
-    console.log(response.url);
+function pageLoad() {
+  try {
+    window.scrollTo(0,0);
+    tryAgain();
+  } catch(e) {
+    setTimeout('pageLoad();', 50);
+    return;
   }
-);
+}
+
+function tryAgain() {
+  if (location.hostname == 'chromewebdata') {
+    var title = this.document.getElementById('tryagain');
+    if (title != null) {
+      return;
+    }
+    retryCount++;
+    retried = true;
+    var content = this.document.getElementById('content');
+    var about = this.document.getElementById('about');
+    var form = this.document.getElementsByTagName('form');
+    var search = this.document.getElementById('search');
+    var scripts = this.document.getElementsByTagName('script');
+    if (scripts) {
+      script = document.createElement("script");
+      script.setAttribute("src", extPath + "tryagain.js?" + Math.random()*1000);
+      scripts[0].parentNode.appendChild(script);
+      link = document.createElement("link");
+      link.setAttribute("rel", "stylesheet");
+      link.setAttribute("href", extPath + "tryagain.css");
+      scripts[0].parentNode.appendChild(link);
+    }
+    var query = url;
+    if (search) {
+      query = search.getAttribute("value");
+      search.setAttribute("onfocus", "stopRetry();");
+      var input = search;
+      while (input != null) {
+        if (input.type == "submit") {
+          input.setAttribute("onclick", "stopRetry();");
+        }
+        input = input.nextSibling;
+      }
+    }
+    if (form) {
+      form = form[0];
+    } else {
+      form = document.createElement("form");
+      form.setAttribute("method", "get");
+      form.setAttribute("action", "http://google.com/search");
+      input = document.createElement("input");
+      input.name = "q";
+      input.setAttribute("id", "search");
+      input.setAttribute("type", "text");
+      input.setAttribute("value", query);
+      form.appendChild(input);
+      input = document.createElement("input");
+      input.setAttribute("type", "submit");
+      input.setAttribute("value", "Google Search");
+      form.appendChild(input);
+    }
+    // clear contents
+    content.innerHTML = "";
+    // stylize message
+    h1 = document.createElement("h1");
+    h1.setAttribute('id', 'tryagain');
+    h1.innerHTML = "Server not found";
+    content.appendChild(h1);
+    div = document.createElement("div");
+    div.className = "small pad";
+    div.innerHTML = "Try " + retryCount + ".";
+    content.appendChild(div);
+    div = document.createElement("div");
+    div.innerHTML = "Chrome can't find the server at " + domain + ".";
+    content.appendChild(div);
+    hr = document.createElement("hr");
+    content.appendChild(hr);
+    // search box
+    content.appendChild(form);
+    hr = document.createElement("hr");
+    content.appendChild(hr);
+    // countdown text
+    div = document.createElement("div");
+    div.innerHTML = "If at first you don't succeed... ";
+    span1 = document.createElement("span");
+    span1.className = "small";
+    span1.setAttribute('id', 'countdown');
+    div.appendChild(span1);
+    span2 = document.createElement("span");
+    span2.setAttribute('id', 'action');
+    div.appendChild(span2);
+    content.appendChild(div);
+    // buttons
+    div = document.createElement("div");
+    div.setAttribute('id', 'buttons');
+    input = document.createElement("input");
+    input.setAttribute('id', 'errorTryAgain');
+    input.setAttribute("onclick", "retryThisExtended();");
+    input.setAttribute("type", "button");
+    input.setAttribute("value", "Try Again");
+    div.appendChild(input);
+    input = document.createElement("input");
+    input.setAttribute('id', 'errorStopRetry');
+    input.setAttribute("onclick", "stopRetry();");
+    input.setAttribute("type", "button");
+    input.setAttribute("value", "Stop trying");
+    div.appendChild(input);
+    input = document.createElement("input");
+    input.setAttribute("onclick", "autoRetryThis();");
+    input.setAttribute("type", "button");
+    input.setAttribute("value", url);
+    input.setAttribute("id", "errorRetry");
+    input.style.display = 'none';
+    // setTimeout(function() { input.click(); }, 100);
+    div.appendChild(input);
+    content.appendChild(div);
+    about.parentNode.removeChild(about);
+    chrome.extension.sendRequest({retry: retryCount});
+  } else {
+    retry = 1;
+  }
+}
+
+pageLoad();
